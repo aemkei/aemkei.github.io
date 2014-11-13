edae183d36d290d9747f4b8bdf07890c11fbf557
Martin Kleppe <kleppe@ubilabs.net>
2014-11-13 23:12:08 +0100

    Cleeanup

--- matrix/escape.js
+++ matrix/escape.js
@@ -3,3 +3,6 @@ escape("允兔充兢兤偐偡偑").replace(/%../g, "\\")
 escape("慢捤").replace(/%./g, "").split()
 
 escape("慬敲琨ㄩ").replace(/([0-9A-F]{2})/g, "\\x$1").replace(/%./g,'')
+
+
+escape("慬敲琨ㄩ").replace(/%./g, "").replace(/(..)/g, "\\x$1")
