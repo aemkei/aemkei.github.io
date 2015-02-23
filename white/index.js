8b1db9b598348e39ed8934ff57751bfbc3c6c710
Martin Kleppe <kleppe@ubilabs.net>
2015-02-23 19:32:29 +0100

    Golf bootstrap code.

---
+++ white/index.js
@@ -0,0 +1,48 @@
+A = ""                    // ""
+
+T = !A + A                // "true"
+L = !T + A                // "false"
+O = A + {}                // "[object Object]"
+
+R = "\\"
+M = '"'
+
+t = T[0]                  // t = "true"[0]
+r = T[1]                  // r = "true"[1]
+
+c = O[5]                  // c = "[object Object]"[5]
+n = (T.L + O)[1]          // n = "undefined[object Object]"[1]
+u = T[2]                  // u = "true"[2]
+o = O[1]                  // o = "[object Object]"[1]
+s = L[3]                  // s = "false"[3]
+e = T[3]                  // e = "true"[3]
+a = L[1]
+
+p = R+160
+
+K = r+e+t+u+r+n + " "
+
+X = T[
+  P = c+o+n+s+t+r+u+c+t+o+r
+][P]
+
+
+X(
+  X(
+   K + M +
+    X(
+      K +
+      X(
+        K + M +
+        e+s+c+a+p+e+
+        "('')." +
+        r+e+p+R+154+a+c+e+      // replace
+        "(/..(.)..(.)/" + 
+        R+ "147,'" +
+        R + R + R + R + R + "170$1$2')" +
+        M
+      )()
+    )()
+    + M
+ )()
+)()
