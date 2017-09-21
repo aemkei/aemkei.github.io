1e7c1fd0d8665273a13c65f850e8849ce31ab880
Martin Kleppe <kleppe@ubilabs.net>
2017-09-21 23:49:53 +0200

    add maze color

--- maze/layout.js
+++ maze/layout.js
@@ -1,42 +1,46 @@
 /*eslint-disable */
 
-s = `S="\\40";M="map";for(z=[i=991];i--;)z[i]=i%30?8:"\\n";F=d=>[30,1,-30,-1][M]((f,g,h)=>S<z[f=d+2*h[3&g+i]]&&(z[f]=z[(d+f)/2]=S,i--,F(f))),F(32),z+""`;
+code = `S="\\40\\n";M="map";i=991;for(z=[];i;)z[--i]=i%30?8:S[1];F=d=>[30,1,-30,-1][M]((f,g,h)=>S[0]<z[f=d+2*h[3&g+i]]&&(z[f]=z[(d+f)/2]=S[0],i--,F(f))),F(32),z+""`;
 
-x = `
-  8888888888888888888888888
-  8       8           8   8
-  8   8   8   8   8   8   8
-  8   8   8   8   8       8
-  8   8   8   8   888888888
-  8       8   8   8       8
-  8   88888   8   8   8   8
-  8   8       8       8   8
-  8   888888888   88888   8
-  8           8       8   8
-  888888888   888888888   8
-  8                       8
-  8888888888888888888888888
-`;
 
-s = s.split("")
+X = (r,c,o=2*c+2,i=2*r*o+o,z=[],F=(p,i=Math.random()*4)=>[o,1,-o,-1].map((s,j,d)=>z[s=p+2*d[j+i&3]]>0&&(z[s]=z[(p+s)/2]=' ',F(s))))=>{for(;i--;)z[i]=i%o?8:`\n`;F(o+2);return''+z}
 
-i = 0;
-y = "";
 
-missing = 0;
 
-for (var a in x){
-  c = x[a]
+while (true) {
 
-  if (c == " "){
-    y += " ";
-  } else if (c == '\n'){
-    y += "\n";
-  } else {
-    y += s.shift() || (missing++, "");
+  x= X(7,7)
+    .replace(/8,8/g,888)
+    .replace(/8,8/g,888)
+    .replace(/,/g, " ");
+
+  s = code.split("")
+
+  i = 0;
+  y = "";
+
+  missing = 0;
+
+  for (var a in x){
+    c = x[a]
+
+    if (c == " "){
+      y += " ";
+    } else if (c == '\n'){
+      y += "\n";
+    } else {
+      y += s.shift() || (missing++, "");
+    }
   }
+
+  y = y + s.join('');
+
+  try {
+    eval(y);
+    console.log(y)
+    // continue;
+  } catch(e) {
+
+  }
+
 }
-
-console.log(y + s.join(''));
-
-console.log(missing)
