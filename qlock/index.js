1e29bd771328a1d07a859359ef13c49c3899d7bc
Martin Kleppe <kleppe@ubilabs.net>
2024-05-13 23:15:36 +0200

    qlock v1

--- qlock/index.js
+++ qlock/index.js
@@ -17,16 +17,15 @@ console.log(o);
 function r(){
   j=o='';
   r=(r+"").replace(/\s/g, '');
+  r=(r+"").replace(/筯ᝉ抧珏寉禎积犒篯篏А/g, '');
   r=r+r;
   for (y=5; y--;) {
     o+="\n";
-    for (x=32;x--;) {
-      d = x/4 | 0;
-      i = x%4 + y*3;
+    for (x=64;x--;) {
+      d = x/8 | 0;
+      i = (x/2|0)%4 + y*3;
       t = (D = Date()[23-d]) < ':' ? D : 10;  
-      c = x%4<3 && '筯ᝉ抧珏寉禎积犒篯篏А'.charCodeAt(t) & 1 << i;  
-      j++;
-      o+= c?r[j]: ' '
+      c = (x/2)%4<3 && '筯ᝉ抧珏寉禎积犒篯篏А'.charCodeAt(t) & 1 << i;  
       j++;
       o+= c?r[j]: ' '
     }
