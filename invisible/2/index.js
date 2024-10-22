499f9509181f50c9d68214172e547f2c126f1d48
Martin Kleppe <kleppe@ubilabs.net>
2024-10-22 22:49:27 +0200

    invisible 114b

---
+++ invisible/2/index.js
@@ -0,0 +1,3 @@
+new Proxy({},{get:(_,n)=>eval([...n].map(n=>+("O">n)).join``.replace(/.{8}/g,n=>String.fromCharCode(+("0b"+n))))}).
+
+OIIOOOOIOIIOIIOOOIIOOIOIOIIIOOIOOIIIOIOOOOIOIOOOOOIIOOOIOOIOIOOI
