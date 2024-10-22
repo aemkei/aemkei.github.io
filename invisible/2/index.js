5a8ec88d68288a7bfcf8f37d7e7dbab1aeb138f2
Martin Kleppe <kleppe@ubilabs.net>
2024-10-22 23:03:55 +0200

    format invisible 2

--- invisible/2/index.js
+++ invisible/2/index.js
@@ -1,3 +1,3 @@
-new Proxy({},{get:(_,n)=>eval([...n].map(n=>+("O">n)).join``.replace(/.{8}/g,n=>String.fromCharCode(+("0b"+n))))}).
+new Proxy({},{get:(_,n)=>eval([...n].map(n=>+("ﾠ">n)).join``.replace(/.{8}/g,n=>String.fromCharCode(+("0b"+n))))}).
 
-OIIOOOOIOIIOIIOOOIIOOIOIOIIIOOIOOIIIOIOOOOIOIOOOOOIIOOOIOOIOIOOI
+ﾠㅤㅤﾠﾠﾠﾠㅤﾠㅤㅤﾠㅤㅤﾠﾠﾠㅤㅤﾠﾠㅤﾠㅤﾠㅤㅤㅤﾠﾠㅤﾠﾠㅤㅤㅤﾠㅤﾠﾠﾠﾠㅤﾠㅤﾠﾠﾠﾠﾠㅤㅤﾠﾠﾠㅤﾠﾠㅤﾠㅤﾠﾠㅤ
